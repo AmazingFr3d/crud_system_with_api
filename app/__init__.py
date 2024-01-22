a32d85f9f85fa2b4cae6895d731e85c4985f002d
Inye Fredrick <amazingfred4real@gmail.com>
2024-01-22 23:29:56 +0100

    rename entry point

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_session import Session
+from flask_session.__init__ import Session
 from flask_restful import Api
 from flask_mail import Mail
 from .config import Config
