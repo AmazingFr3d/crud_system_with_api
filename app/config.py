f734e92eec03ee32203bfc5a7531fcdb31fc37fd
Inye Fredrick <amazingfred4real@gmail.com>
2024-01-23 13:51:33 +0100

    add sk to env

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
         hostname=os.getenv("DB_HOST"),
         databasename=os.getenv("DB_NAME"),)
     # SQLALCHEMY_DATABASE_URI = "mysql+pymsql://Tolumichael:wS.4j8q9CmXv.!m@http://Tolumichael.mysql.pythonanywhere-services.com/Tolumichael$excel_db"
-    SECRET_KEY = '9c97f13c5fd005af6d6090af'
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SESSION_TYPE = 'filesystem'
     SESSION_FILE_DIR = '/tmp/flask_session'
     MAIL_SERVER = 'smtp.gmail.com'  # Update with your email provider's SMTP server
