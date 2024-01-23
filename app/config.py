03b6368ed5f8606440712f73654e07f023e4eb94
Inye Fredrick <amazingfred4real@gmail.com>
2024-01-23 13:41:14 +0100

    add env variables

--- app/config.py
+++ app/config.py
@@ -1,10 +1,12 @@
+import os
+
 class Config:
     # SQLALCHEMY_DATABASE_URI = "sqlite:///dbmgt.sqlite"
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username="Tolumichael",
-        password="wS.4j8q9CmXv.!m",
-        hostname="Tolumichael.mysql.pythonanywhere-services.com",
-        databasename="Tolumichael$excel_db",)
+        username=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        hostname=os.getenv("DB_HOST"),
+        databasename=os.getenv("DB_NAME"),)
     # SQLALCHEMY_DATABASE_URI = "mysql+pymsql://Tolumichael:wS.4j8q9CmXv.!m@http://Tolumichael.mysql.pythonanywhere-services.com/Tolumichael$excel_db"
     SECRET_KEY = '9c97f13c5fd005af6d6090af'
     SESSION_TYPE = 'filesystem'
@@ -13,6 +15,6 @@ class Config:
     MAIL_PORT = 465
     MAIL_USE_SSL = False
     MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
-    MAIL_DEFAULT_SENDER = ''
+    # MAIL_USERNAME = ''
+    # MAIL_PASSWORD = ''
+    # MAIL_DEFAULT_SENDER = ''
